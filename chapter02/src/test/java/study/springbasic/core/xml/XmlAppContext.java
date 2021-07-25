5bfec51620ea211e3d9d062b95d899c7ef004e7b
Ddudooo <igoa.work@gmail.com>
2021-07-25 23:05:05 +0900

    xml 설정 방식 테스트 추가

---
+++ chapter02/src/test/java/study/springbasic/core/xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package study.springbasic.core.xml;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import study.springbasic.core.member.MemberService;
+
+public class XmlAppContext {
+
+	@Test
+	void xmlAppContext() {
+		ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+		MemberService memberService = ac.getBean("memberService", MemberService.class);
+		assertThat(memberService).isInstanceOf(MemberService.class);
+	}
+}
