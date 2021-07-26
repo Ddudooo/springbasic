1108a4c2f23740008568417b520f0c0a0666f287
Ddudooo <igoa.work@gmail.com>
2021-07-26 13:15:23 +0900

    @Configuration과 바이트코드 조작의 마법
    스프링 설정 정보는 @Configuration을 사용하자
    
    @Configuration을 사용안할시 싱글톤을 보장하지 않음.

--- chapter02/src/test/java/study/springbasic/core/singleton/ConfigurationSingletonTest.java
+++ chapter02/src/test/java/study/springbasic/core/singleton/ConfigurationSingletonTest.java
@@ -32,4 +32,11 @@ public class ConfigurationSingletonTest {
 		assertThat(orderService.getMemberRepository())
 			.isSameAs(memberRepository);
 	}
+
+	@Test
+	void configurationDeep() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+		AppConfig bean = ac.getBean(AppConfig.class);
+		System.out.println("bean = " + bean.getClass());
+	}
 }
