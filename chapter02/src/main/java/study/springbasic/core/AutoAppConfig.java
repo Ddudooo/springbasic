6e27b6794fe595bbe384a62f62a535502dd02c7e
Ddudooo <igoa.work@gmail.com>
2021-07-26 15:47:06 +0900

    중복 등록과 충돌

--- chapter02/src/main/java/study/springbasic/core/AutoAppConfig.java
+++ chapter02/src/main/java/study/springbasic/core/AutoAppConfig.java
@@ -1,8 +1,11 @@
 package study.springbasic.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import study.springbasic.core.member.MemberRepository;
+import study.springbasic.core.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
@@ -12,4 +15,8 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+	@Bean(name = "memoryMemberRepository")
+	public MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
 }
