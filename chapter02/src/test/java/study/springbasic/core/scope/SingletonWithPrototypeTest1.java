610e9d3462bf584e8adf5ce4253fd5e551491adb
Ddudooo <igoa.work@gmail.com>
2021-07-27 14:21:42 +0900

    프로토타입 스코프 와 싱글톤 빈 혼합 사용
    싱글톤에서 프로토타입을 주입받을때 문제점
    
    프로토타입 목적에 맞춰 새로 생성되는것이 아니라 주입받은 빈으로 유지

---
+++ chapter02/src/test/java/study/springbasic/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,94 @@
+package study.springbasic.core.scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototypeTest1 {
+
+	@Test
+	void prototypeFind() {
+		ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(
+			PrototypeBean.class);
+
+		PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+		bean1.addCount();
+		assertThat(bean1.getCount()).isEqualTo(1);
+
+		PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+		bean2.addCount();
+		assertThat(bean2.getCount()).isEqualTo(1);
+
+		ac.close();
+	}
+
+	@Test
+	void singletonClientUseProtoType() {
+		ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(
+			PrototypeBean.class, ClientBean.class);
+
+		ClientBean client1 = ac.getBean(ClientBean.class);
+		int count1 = client1.logic();
+		assertThat(count1).isEqualTo(1);
+
+		ClientBean client2 = ac.getBean(ClientBean.class);
+		int count2 = client2.logic();
+		assertThat(count2).isEqualTo(2);
+	}
+
+	@Scope("prototype")
+	static class PrototypeBean {
+
+		private int count = 0;
+
+		public void addCount() {
+			count++;
+		}
+
+		public int getCount() {
+			return count;
+		}
+
+		@PostConstruct
+		public void init() {
+			System.out.println("PrototypeBean.init " + this);
+		}
+
+		@PreDestroy
+		public void destroy() {
+			System.out.println("PrototypeBean.destroy " + this);
+		}
+	}
+
+	static class ClientBean {
+
+		private PrototypeBean prototypeBean;
+
+		@Autowired
+		public ClientBean(
+			PrototypeBean prototypeBean) {
+			this.prototypeBean = prototypeBean;
+		}
+
+		public int logic() {
+			prototypeBean.addCount();
+			return prototypeBean.getCount();
+		}
+
+		@PostConstruct
+		public void init() {
+			System.out.println("ClientBean.init " + this);
+		}
+
+		@PreDestroy
+		public void destroy() {
+			System.out.println("ClientBean.destroy " + this);
+		}
+	}
+}
